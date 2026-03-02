2a4211047dcfe2e570a759b00721612987556802
Wesley Neves <wesleyneves675@gmail.com>
2026-03-02 16:45:16 -0400

    finalização da chamada de banco e configuração da chamada do header

--- src/banco.php
+++ src/banco.php
@@ -1,21 +1,26 @@
 <?php
 $servidor = "localhost";
 $usuario = "root";
-$senha = "";
+$senha = "root";
 $banco = "estoque";
 
 // Conexão com o banco de dados usando PDO
 try {
-    $conexao = new PDO("
-    mysql:host=$servidor; 
-    dbname=$banco;
-    charset=utf8",
-    $usuario,
-    $senha);
+    $conexao = new PDO("mysql:host=$servidor; dbname=$banco; charset=utf8", $usuario,$senha);
 
     // Configura o modo de erro do PDO para exceção
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Configura o modo de busca padrão para associativo
+    $conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    //echo "conexao bem sucedida";
+
 } catch (\Throwable $erro) {
-    // Exibe a mensagem de erro e encerra o script
-    echo "Erro ao conectar ao banco de dados: " . $erro->getMessage();
-    exit;
+
+    // Exibe uma mensagem genérica para o usuário
+    die("Erro ao conectar ao banco de dados: " . $erro->getMessage());
+
 }
+
+
